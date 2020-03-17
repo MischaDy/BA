de68e8e30e7df26a711df873b290a63b9ae6c4f3
Mischa <michaeldubovoy@web.de>
2020-03-17 17:04:02 +0100

    Updated gitignore. Started woking on testing different clustering algorithms.

--- Logic/my_test/face_evoLVe Test/my_face_evoLVe_test.py
+++ Logic/my_test/face_evoLVe Test/my_face_evoLVe_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c01b43e47bf66aa3df55aebb995790aa9effb20671e4e132e35ef304a7691786
-size 1487
+oid sha256:50a7fd80fb35b419c53239751bc8fe05d0c5a0ad3da2b491a72224fec34faaa4
+size 2560

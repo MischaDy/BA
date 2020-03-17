93896d11e803bf1279f79b986f5b2ac3968b66cc
Mischa <michaeldubovoy@web.de>
2020-03-17 17:53:55 +0100

    Continued working on tests

--- Logic/my_model/model_test.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c8fd61ea8f5c0fa7cfbcb54326743cf5df19affdfbbd82dbfea5a2b26693c28f
-size 134
---
+++ Logic/my_test/face_evoLVe Test/my_model/model_test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e90ffaff051a2dd8b6ccbec6a0e24b0141ed7690ee6d1fe198bb469fa3013e79
+size 2513

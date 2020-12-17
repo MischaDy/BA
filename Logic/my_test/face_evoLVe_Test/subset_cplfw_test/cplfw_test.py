d396c3dbe42281347e3ff356dc08209a05424cf1
Mischa <michaeldubovoy@web.de>
2020-12-17 19:04:26 +0100

    Had to reinput my py-files, since LFS isn't tracking them anymore. (Why did it in the first place??)

--- Logic/my_test/face_evoLVe_Test/subset_cplfw_test/cplfw_test.py
+++ Logic/my_test/face_evoLVe_Test/subset_cplfw_test/cplfw_test.py
@@ -1,3 +1,70 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:140980fb068378f42437e97b17b7b9a33a2e5cfc94148e0d14523b5118088e91
-size 2558
+import torch
+from torchvision import transforms
+from Logic.face_evoLVe_PyTorch.backbone.model_irse import IR_50
+from facenet_pytorch import MTCNN
+from PIL import Image
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+MODEL_PATH = '../my_model/backbone_ir50_ms1m_epoch63.pth'
+PIL_TO_TENSOR = transforms.ToTensor()
+
+
+def compute_dist_matrix(vectors):
+    num_vectors = len(vectors)
+    dist_matrix = torch.zeros(num_vectors, num_vectors)
+
+    for ind1, vector1 in enumerate(vectors):
+        for ind2, vector2 in enumerate(vectors):
+            if ind2 <= ind1:
+                continue
+            cur_dist = vector1.dist(vector2)
+            dist_matrix[ind1][ind2] = cur_dist
+            dist_matrix[ind2][ind1] = cur_dist
+
+    return dist_matrix
+
+
+def show_dist_matrix(dist_matrix):
+    plt.imshow(dist_matrix.detach().numpy(), cmap=cm.YlOrRd)
+    plt.colorbar()
+    plt.show()
+
+
+input_size = [112, 112]
+
+mtcnn = MTCNN(image_size=input_size[0], margin=0)
+
+device = torch.device('cpu')
+model = IR_50(input_size)
+model.load_state_dict(torch.load(MODEL_PATH, map_location='cpu'))
+model.eval()
+
+
+# img_old_clooney = Image.open("../age_imgs/old-george-clooney.jpg")
+# img_younger_clooney = Image.open("../age_imgs/younger-george-clooney.jpg")
+# img_lookalike_clooney = Image.open("../age_imgs/lookalike-george-clooney.jpg")
+
+# # Get cropped and pre-whitened image tensor
+# img_old_cropped = mtcnn(img_old_clooney, save_path="../age_testrun_imgs/img_old_cropped.jpg")  # mtcnn
+# img_younger_cropped = mtcnn(img_younger_clooney, save_path="../age_testrun_imgs/img_younger_cropped.jpg")
+# img_lookalike_cropped = mtcnn(img_lookalike_clooney, save_path="../age_testrun_imgs/img_lookalike_cropped.jpg")
+
+# # Get cropped and pre-whitened image tensor
+img_old_cropped = PIL_TO_TENSOR(Image.open("../age_testrun_imgs/img_old_cropped.jpg"))
+img_younger_cropped = PIL_TO_TENSOR(Image.open("../age_testrun_imgs/img_younger_cropped.jpg"))
+img_lookalike_cropped = PIL_TO_TENSOR(Image.open("../age_testrun_imgs/img_lookalike_cropped.jpg"))
+
+
+# Calculate embedding (unsqueeze to add batch dimension)
+img_old_embedding = model(img_old_cropped.unsqueeze(0))
+img_younger_embedding = model(img_younger_cropped.unsqueeze(0))
+img_lookalike_embedding = model(img_lookalike_cropped.unsqueeze(0))
+
+
+embeddings_list = (img_old_embedding, img_younger_embedding, img_lookalike_embedding)
+embeddings_dist_matrix = compute_dist_matrix(embeddings_list)
+# vector_names = ("Old Clooney", "Younger Clooney", "Lookalike")
+show_dist_matrix(embeddings_dist_matrix)  # , vector_names)
--- Logic/my_test/facenet_Test/subset_cplfw_test/image_loader/pytorch_image_folder_with_file_paths.py
+++ Logic/my_test/facenet_Test/subset_cplfw_test/image_loader/pytorch_image_folder_with_file_paths.py
@@ -1,3 +1,36 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8c86af1a9462aa9252bb9ba91ce25286cd6970e09870efa5fe01b8f4321453f9
-size 1252
+# https://gist.github.com/andrewjong/6b02ff237533b3b2c554701fb53d5c4d
+import torch
+from torchvision import datasets, transforms
+
+
+class ImageFolderWithPaths(datasets.ImageFolder):
+    """Custom dataset that includes image file paths.
+    Extends torchvision.datasets.ImageFolder.
+    """
+
+    # override the __getitem__ method. this is the method that dataloader calls
+    def __getitem__(self, index):
+        # this is what ImageFolder normally returns 
+        original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
+        # the image file path
+        path = self.imgs[index][0]
+        # make a new tuple that includes image of original and the path
+        tuple_with_path = (original_tuple[0], path)
+        return tuple_with_path
+
+
+def load_data(data_dir):
+    # instantiate the dataset and dataloader
+    dataset = ImageFolderWithPaths(data_dir, transform=transforms.ToTensor())  # our custom dataset
+    dataloader = torch.utils.data.DataLoader(dataset)
+    return dataloader
+
+
+if __name__ == '__main__':
+    data_dir = "your/data_dir/here"
+    dataloader = load_data(data_dir)
+
+    # iterate over data
+    for inputs, labels, paths in dataloader:
+        # use the above variables freely
+        print(inputs, labels, paths)

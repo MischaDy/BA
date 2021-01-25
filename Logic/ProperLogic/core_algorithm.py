72cd903b1536eb5dfb91d79087d1c08bccdc489a
Mischa <michaeldubovoy@web.de>
2021-01-25 22:27:32 +0100

    Implemented Clustersplitting! TODO: 1. Clustersplitting when center moves too far away?  2. Start reclustering with distant embedding!

--- Logic/ProperLogic/core_algorithm.py
+++ Logic/ProperLogic/core_algorithm.py
@@ -1,3 +1,5 @@
+import shutil
+
 from input_output_logic import *
 import torch
 
@@ -9,16 +11,22 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-MAX_NUM_CLUSTER_COMP = 10  # maximum number of clusters to compute distance to
-CLASSIFICATION_THRESHOLD = 0.53  # OR 0.73 cf. Bijl - A comparison of clustering algorithms for face clustering
-#_NUM_EMBEDDINGS_TO_CLASSIFY = -1
+CLASSIFICATION_THRESHOLD = 0.95  # 0.53  # OR 0.73 cf. Bijl - A comparison of clustering algorithms for face clustering
+RECLUSTERING_THRESHOLD = 2 * CLASSIFICATION_THRESHOLD  # max dist to cluster center before an embed. gets own cluster
+
+MAX_CLUSTER_SIZE = 64
+_MAX_NUM_TOTAL_COMPS = 1000
+MAX_NUM_CLUSTER_COMPS = _MAX_NUM_TOTAL_COMPS // MAX_CLUSTER_SIZE  # maximum number of clusters to compute distance to
 
 CLUSTERS_PATH = 'stored_clusters'
 EMBEDDINGS_PATH = 'stored_embeddings'
 
 
-# TODO: Proper comments!
-# TODO: Store thumbnail imgs WITH the tensors
+# TODO: remove
+_NUM_EMBEDDINGS_TO_CLASSIFY = 100
+
+
+# TODO: Add proper comments!
 # TODO: Make embedding id 'proper' id?? ---> Also in input_output_logic!
 
 # TODO: Decide on exact way to split cluster??
@@ -65,11 +73,14 @@ class Cluster:
     def get_embeddings(self, return_embeddings_ids=False):
         if return_embeddings_ids:
             return self.embeddings
-        return self.embeddings.values()
+        return list(self.embeddings.values())
 
     # def get_embedding_id(self, embedding):
     #     # TODO: implement(?)
     #     return ...
+
+    def get_size(self):
+        return len(self.embeddings)
 
     def add_embedding(self, embedding, embedding_id=None):
         if embedding_id is None:
@@ -119,7 +130,7 @@ class Cluster:
         :param cluster_id:
         :return:
         """
-        embeddings = list(load_embeddings_from_path(path_to_cluster))
+        embeddings = list(load_tensors(path_to_cluster, from_path=True))
         cluster = cls(embeddings)
         if cluster_id is not None:
             cluster.cluster_id = cluster_id
@@ -129,23 +140,23 @@ class Cluster:
     def sum_embeddings(embeddings):
         return reduce(torch.add, embeddings)
 
-    # def compute_avg_dist_to_center(self, dim=None):
-    #     pass
 
-
-def main_algorithm(embeddings_path, classification_threshold, max_num_cluster_comp, cluster_save_path=None):
+def cluster_embeddings(embeddings, classification_threshold, max_num_cluster_comps, reclustering_threshold=None,
+                       max_cluster_size=None, cluster_save_path=None):
     """
     Build clusters from face embeddings stored in the given path using the specified classification threshold.
     (Currently handled as: All embeddings closer than the distance given by the classification threshold are placed in
     the same cluster. If cluster_save_path is set, store the resulting clusters as directories in the given path.
 
-    :param max_num_cluster_comp:
-    :param embeddings_path:
+    :param embeddings: Either a path (string) to the directory wherein embeddings are located or an iterable containing
+    the embeddings
     :param classification_threshold:
+    :param max_num_cluster_comps:
+    :param max_cluster_size:
     :param cluster_save_path:
     :return:
     """
-    embeddings_loader = load_embeddings_from_path(embeddings_path, yield_paths=True)
+    embeddings_loader = load_tensors(embeddings, yield_paths=True, from_path=isinstance(embeddings, str))
     try:
         first_file_path, first_embedding = next(embeddings_loader)
     except StopIteration as error:
@@ -157,21 +168,25 @@ def main_algorithm(embeddings_path, classification_threshold, max_num_cluster_co
     # iterate over remaining embeddings
     logging.info('START iteration over embeddings')
     time1 = default_timer()
-    # counter_vals = range(2, _NUM_EMBEDDINGS_TO_CLASSIFY + 1) if _NUM_EMBEDDINGS_TO_CLASSIFY >= 0 else count(2)
-    # zip(counter_vals, embeddings_loader):
-    for counter, (embedding_file_path, new_embedding) in enumerate(embeddings_loader, start=2):
+    counter_vals = range(2, _NUM_EMBEDDINGS_TO_CLASSIFY + 1) if _NUM_EMBEDDINGS_TO_CLASSIFY >= 0 else count(2)
+    for counter, (embedding_file_path, new_embedding) in zip(counter_vals, embeddings_loader):  # enumerate(embeddings_loader, start=2):
         if counter % 100 == 0:
             logging.info(f' --- Current embedding number: {counter}')
 
         # sort clusters by distance from their center to embedding; only consider closest clusters
         clusters_by_center_dist = sorted(clusters, key=lambda cluster: cluster.compute_dist_to_center(new_embedding))
-        closest_clusters = clusters_by_center_dist[:max_num_cluster_comp]
+        closest_clusters = clusters_by_center_dist[:max_num_cluster_comps]
 
         shortest_emb_dist, closest_cluster = find_closest_cluster_to_embedding(closest_clusters, new_embedding)
         embedding_file_name = os.path.split(embedding_file_path)[-1]
 
         if shortest_emb_dist <= classification_threshold:
             closest_cluster.add_embedding(new_embedding, embedding_file_name)
+            if should_recluster(closest_cluster, new_embedding, max_cluster_size, reclustering_threshold):
+                new_clusters = recluster_without_splitting(closest_cluster.get_embeddings(), classification_threshold,
+                                                           max_num_cluster_comps)
+                clusters.extend(new_clusters)
+                clusters.remove(closest_cluster)
         else:
             clusters.append(Cluster([new_embedding], [embedding_file_name]))
 
@@ -189,6 +204,16 @@ def main_algorithm(embeddings_path, classification_threshold, max_num_cluster_co
         logging.info(f' --- Last cluster number: {counter}')
         logging.info(f'END cluster saving')
         logging.info(f'Time spent on saving clusters: {default_timer() - time1}')
+    else:
+        return clusters
+
+
+def should_recluster(cluster, embedding, max_cluster_size, reclustering_threshold):
+    if max_cluster_size is not None and cluster.get_size() >= max_cluster_size:
+        return True
+    if reclustering_threshold is not None:
+        return cluster.compute_dist_to_center(embedding) >= reclustering_threshold
+    return False
 
 
 def find_closest_cluster_to_embedding(clusters, embedding, return_dist=True):
@@ -213,6 +238,14 @@ def find_closest_cluster_to_embedding(clusters, embedding, return_dist=True):
     if return_dist:
         return shortest_emb_dist, closest_cluster
     return closest_cluster
+
+
+def recluster_without_splitting(embeddings, threshold, max_num_cluster_comp):
+    # TODO: Start with newly added embedding!
+    return cluster_embeddings(embeddings, threshold, max_num_cluster_comp)
+
+
+# ------- HELPERS -------
 
 
 def log_error(msg):
@@ -240,5 +273,24 @@ def compute_dist(embedding1, embedding2):
     return float(torch.dist(embedding1, embedding2))
 
 
+def remove_directory_trees(dir_tree_paths):
+    for dir_tree_path in dir_tree_paths:
+        shutil.rmtree(dir_tree_path, onerror=_handle_tree_removal_errors)
+
+
+def _handle_tree_removal_errors(function, path, excinfo):
+    exc_type, _, traceback = excinfo
+    if exc_type is FileNotFoundError:
+        log_error(f'File or directory at {path} not found')
+    elif exc_type is PermissionError:
+        log_error(f'No permission to remove file or directory at {path}')
+    elif exc_type is OSError:
+        log_error(f'The following error occured for the file or directory at {path}:' '\n', traceback)
+    else:
+        raise
+
+
 if __name__ == '__main__':
-    main_algorithm(EMBEDDINGS_PATH, CLASSIFICATION_THRESHOLD, MAX_NUM_CLUSTER_COMP, CLUSTERS_PATH)
+    remove_directory_trees([CLUSTERS_PATH])
+    cluster_embeddings(EMBEDDINGS_PATH, CLASSIFICATION_THRESHOLD, MAX_NUM_CLUSTER_COMPS, RECLUSTERING_THRESHOLD,
+                       MAX_CLUSTER_SIZE, CLUSTERS_PATH)

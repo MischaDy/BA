3de5829562c335a23a27a6dbfbe87b3a33a51811
Mischa <michaeldubovoy@web.de>
2021-05-01 15:33:38 +0200

    [Untested] Added plots and visualization for commands stats

--- Logic/evaluate_performance/visualization/visualize_cmd_stats.py
+++ Logic/evaluate_performance/visualization/visualize_cmd_stats.py
@@ -6,131 +6,67 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
-from Logic.ProperLogic.misc_helpers import get_every_nth_item, group_pairs, map_dict_vals, log_error
+from Logic.ProperLogic.misc_helpers import get_every_nth_item, group_pairs, map_dict_vals, log_error, get_ext
 
-RESULTS_DIR_PATH = '../results_thresholds2'
-YVALUE = 'precision'  # 'f-measure'
-
-
-def main(results_dir_path):
-    thres_and_met_to_f_measure = get_thres_and_met_to_f_measure(results_dir_path)
-    plot_f_measure_vs_param(thres_and_met_to_f_measure, thres=True)
-    # plot_f_measure_vs_param(thres_and_met_to_f_measure, metric=True)
-    # plot_f_measure_vs_threshold_and_metric(thres_and_met_to_f_measure)
+RESULTS_DIR_PATH = '../results_cmd_stats'
+CMD_STATS_FILE = r'C:\Users\Mischa\Desktop\Uni\20-21 WS\Bachelor\BA Papers\Datasets\faces 1999 caltech\commands_stats.txt'
 
 
-def get_thres_and_met_to_f_measure(results_dir_path):
-    result_files = os.listdir(results_dir_path)
-    result_file_paths = [os.path.join(results_dir_path, result_file) for result_file in result_files]
-    processed_file_names = []
-    for result_file in result_files:
-        file_name_no_ext = os.path.splitext(result_file)[0]
-        processed_file_name = file_name_no_ext.split('___')[1].replace('_point_', '.')
-        processed_file_names.append(processed_file_name)
+def main():
+    with open(CMD_STATS_FILE, 'r') as file:
+        lines = file.readlines()
 
-    file_contents = dict()
-    for file_name, result_path in zip(processed_file_names, result_file_paths):
-        with open(result_path, 'r') as file:
-            lines = file.readlines()
-            file_contents[file_name] = process_lines(lines)
+    cmd_dict = {'process_images_dir': [], 'reclassify': [], 'clear_data': []}
+    for line in lines:
+        cmd_name, n, duration = line.strip('[]\n').split(', ')
+        cmd_name, n, duration = cmd_name.strip(" '"), int(n), float(duration)
+        cmd_dict[cmd_name].append((n, round(duration, 3)))
 
-    thres_and_met_to_f_measure = dict()
-    for thres_and_met, results_dict in file_contents.items():
-        thres_and_met_to_f_measure[thres_and_met] = results_dict[YVALUE]
-
-    return thres_and_met_to_f_measure
+    for cmd_name, xs_and_ys in cmd_dict.items():
+        xs, ys = list(get_every_nth_item(xs_and_ys, 0)), list(get_every_nth_item(xs_and_ys, 1))
+        save_path = os.path.join(RESULTS_DIR_PATH, cmd_name + '.png')
+        plot_cmd(cmd_name, xs, ys, save_path)
 
 
-# def plot_f_measure_vs_metric(thres_and_met_to_f_measure):
-#     print('hi')
-#     met_f_measure = [
-#         (thres_met.split('__')[0], f_measure)
-#         for thres_met, f_measure in thres_and_met_to_f_measure.items()
-#     ]
-#     met_f_measure = [
-#         (met.lstrip('L'), float(f_measure))
-#         for met, f_measure in met_f_measure
-#     ]
-#     # TODO: Incorporate variance, too?
-#
-#     thres_met_f_measures_groups = group_pairs(met_f_measure, ret_dict=True)
-#     map_dict_vals(thres_met_f_measures_groups, func=np.mean)
-#     thres_met_f_measures_groups = sorted(thres_met_f_measures_groups.items())
-#
-#     # grouped_thres_met_f_measures = defaultdict(list)
-#     # for met, f_measures in groupby(met_f_measure, key=lambda mf: mf[0]):
-#     #     grouped_thres_met_f_measures[met].append(np.mean(f_measures))
-#     # grouped_thres_met_f_measures = sorted(grouped_thres_met_f_measures.items())
-#
-#     xs = get_every_nth_item(thres_met_f_measures_groups, n=0)
-#     ys = get_every_nth_item(thres_met_f_measures_groups, n=1)
-#     plt.scatter(list(xs), list(ys))
-#     plt.show()
+def plot_cmd(cmd_name, xs, ys, save_path=None):
+    x_eps = 450 / 20
+    y_eps = max(ys) / 20
+    x_axis_limits = [0 - x_eps, 450 + x_eps]
+    y_axis_limits = [0 - y_eps, max(ys) + y_eps]
 
+    fig, ax = plt.subplots()
+    plt.plot(xs, ys)
+    plt.title(cmd_name)
+    ax.set_xlim(x_axis_limits)
+    ax.set_ylim(y_axis_limits)
+    ax.set_xlabel('number of images processed')
+    ax.set_ylabel('runtime in seconds')
 
-def _plot_worker(xs, ys, xlabel='', ylabel='f-measure', title=''):
-    # fig = plt.figure()
-
-    plt.scatter(list(xs), list(ys))
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
+    if save_path is not None:
+        plt.savefig(save_path, format=get_ext(save_path))
     plt.show()
 
 
-def plot_f_measure_vs_param(thres_and_met_to_f_measure, thres=False, metric=False):
-    # dict_ = get_xs_and_ys(thres_and_met_to_f_measure, thres=thres, metric=metric)
-    # params_and_f_measure_data_frame = pd.DataFrame(dict_)
-    # print(params_and_f_measure_data_frame)
+def _plot_thresholds_vs_params_helper(thresholds_and_params, ylabel, title=None, x_eps=None, y_eps=0.05):
+    thresholds = list(get_every_nth_item(thresholds_and_params, n=0))
+    min_thres, max_thres = min(map(min, thresholds)), max(map(max, thresholds))
+    if x_eps is None:
+        x_eps = (max_thres - min_thres) / 20  # 5% margin
+    x_axis_limits = [min_thres - x_eps, max_thres + x_eps]
+    y_axis_limits = [0 - y_eps, 1 + y_eps]
 
-    xs, ys = get_xs_and_ys(thres_and_met_to_f_measure, thres=thres, metric=metric)
-    xlabel = 'metric' if metric else 'thres'
-    _plot_worker(xs, ys, xlabel)
+    fig, ax = plt.subplots()
+    xlabel = 'Thresholds'
+    if title is None:
+        title = f'Threshold vs. {ylabel}'
 
-
-def get_xs_and_ys(thres_and_met_to_f_measure, thres=False, metric=False):
-    if thres:
-        ind = 1
-    elif metric:
-        ind = 0
-    else:
-        log_error('thres or metric must be true')
-        return
-
-    param_f_measure = [
-        (thres_met.split('__')[ind], f_measure)
-        for thres_met, f_measure in thres_and_met_to_f_measure.items()
-    ]
-    param_f_measure = [
-        (param.lstrip('LT'), float(f_measure))
-        for param, f_measure in param_f_measure
-    ]
-    # TODO: Incorporate variance, too?
-
-    # thres_met_f_measures_groups_dict = group_pairs(param_f_measure, ret_dict=True)
-    # # map_dict_vals(thres_met_f_measures_groups_dict, func=np.mean)
-    # # return thres_met_f_measures_groups_dict
-    #
-    # thres_met_f_measures_groups = sorted(thres_met_f_measures_groups_dict.items())
-
-    # grouped_thres_met_f_measures = defaultdict(list)
-    # for met, f_measures in groupby(met_f_measure, key=lambda mf: mf[0]):
-    #     grouped_thres_met_f_measures[met].append(np.mean(f_measures))
-    # grouped_thres_met_f_measures = sorted(grouped_thres_met_f_measures.items())
-
-    xs = get_every_nth_item(param_f_measure, n=0)
-    ys = get_every_nth_item(param_f_measure, n=1)
-    return list(xs), list(ys)
-
-
-def plot_f_measure_vs_threshold_and_metric(thres_and_met_to_f_measure):
-    pass
-
-
-def process_lines(lines, sep=': '):
-    processed_lines = dict(line.split(sep) for line in lines)
-    return processed_lines
+    plt.title(title)
+    ax.set_xlim(x_axis_limits)
+    ax.set_ylim(y_axis_limits)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    return fig, ax
 
 
 if __name__ == '__main__':
-    main(RESULTS_DIR_PATH)
+    main()
